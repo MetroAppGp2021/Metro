d870ca5575a445f12e4c9c0a166cd6e8d3db5d79
hafsamohamed <36491829+hafsamohamed@users.noreply.github.com>
2021-03-21 07:39:37 +0200

    Initial commit1

---
+++ app/src/main/java/com/example/metrocard2020/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.metrocard2020.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.metrocard2020.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
